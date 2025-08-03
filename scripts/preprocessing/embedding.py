99a0ac4424b9be139d84760a6f7c7a120b420f35
iamaber <abirislam1892@gmail.com>
2025-08-03 23:03:04 +0600

    Add FAISS index path import to embedding script

--- scripts/preprocessing/embedding.py
+++ scripts/preprocessing/embedding.py
@@ -1,8 +1,9 @@
 import json
 import os
+import faiss
 from transformers import AutoTokenizer, AutoModel
 import torch
-from config.settings import MODEL_NAME, PROCESSED_DATA_PATH
+from config.settings import MODEL_NAME, PROCESSED_DATA_PATH, FAISS_INDEX_PATH
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModel.from_pretrained(MODEL_NAME)
@@ -16,3 +17,17 @@ def get_document_embeddings(texts):
         model_output = model(**inputs)
     embeddings = model_output.last_hidden_state[:, 0, :].numpy()
     return embeddings
+
+
+def load_data(directory):
+    documents = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory, filename)
+            with open(filepath, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                if isinstance(data, list):
+                    documents.extend(data)
+                else:
+                    documents.append(data)
+    return documents
