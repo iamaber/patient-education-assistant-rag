afbb8e8704de0e193fff3f4455ca11e9062fd677
iamaber <abirislam1892@gmail.com>
2025-08-04 15:00:00 +0600

    Add embedding generation functionality and update dependencies
    - Implemented document embedding generation in `rag-processes.ipynb` using transformers and FAISS for efficient similarity search.
    - Enhanced `embedding.py` to include batch processing for embeddings and save document metadata alongside the FAISS index.
    - Updated `pyproject.toml` to include new dependencies: `catppuccin-jupyterlab` and `dotenv`.
    - Modified `uv.lock` to reflect the addition of new packages and their versions.

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,6 @@ NCBI_EMAIL = os.getenv("NCBI_EMAIL")
 NCBI_API_KEY = os.getenv("NCBI_API_KEY")
 
 PROCESSED_DATA_PATH = "./data/processed"
-FAISS_INDEX_PATH = "./faiss_index.bin"
+FAISS_INDEX_PATH = "./faiss_index.faiss"
 ES_INDEX_NAME = "medical_documents"
-MODEL_NAME = "bert-base-multilingual-cased"
+MODEL_NAME = "dmis-lab/biobert-base-cased-v1.1"
